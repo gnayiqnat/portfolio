c44a03ccb9b0bc2b02b7e3e501addd128d84426e
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-03-26 01:19:53 +0800

    style: Change About text ellipsis to balance

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -14,7 +14,7 @@ export default function AboutSection() {
 				<h2 className={` text-xl md:text-2xl font-jetbrains font-light`}>
 					{`->~== { about } ==~<-`} {/*{`--->>>~=== { projects } ===~<<<<<---`} */}
 				</h2>
-				<h3 className='text-lg font-jetbrains text-center opacity-80 text-ellipsis'>
+				<h3 className='text-lg font-jetbrains text-center opacity-80 text-balance'>
 					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam minus
 					dignissimos ab unde, reprehenderit nemo. A ipsa voluptas, odit rem delectus
 					nulla asperiores, neque quis, consequatur qui quos dolorum sint.
