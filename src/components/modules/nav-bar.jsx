f140a2a27ba725b1533f7575fae485acd11590fc
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-03-30 00:51:16 +0800

    revert(nav): Change href to '/' to keep consistency

--- src/components/modules/nav-bar.jsx
+++ src/components/modules/nav-bar.jsx
@@ -1,13 +1,13 @@
+import { SearchField } from "@heroui/react";
 import Link from "next/link";
 
-export default function NavBar(n) {
+export default function NavBar() {
 	return (
 		<>
-			<div className='px-10 w-full h-30 flex  justify-center sm:justify-between items-center bg-transparent border-b'>
-				<Link href={`${n.type === 'blog' ? '/blog' : '/'}`}>
-					<span className='font-barcode text-4xl'>TAN QI YANG</span>
+			<div className='px-10 w-full h-23 flex  justify-center sm:justify-between items-center bg-transparent border-b'>
+				<Link href='/'>
+					<span className='font-barcode text-4xl text-gray-700'>TAN QI YANG</span>
 				</Link>
-        <span> </span>
       </div>
 		</>
 	);
