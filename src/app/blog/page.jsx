ec671464265570f26b6274ea06320f10292dba22
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-03-29 21:22:53 +0800

    feat(blog): Add <ReactLenis /> component in blog subpages for smooth scrolling

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,6 +3,7 @@ import BlogCard from '@/components/modules/blog-card';
 import { sanityFetch } from '@/sanity/lib/live';
 import { POSTS_QUERY } from '@/sanity/lib/queries';
 import Link from 'next/link';
+import { ReactLenis } from 'lenis/react';
 
 export default async function IndexPage() {
 	const { data: posts } = await sanityFetch({
@@ -11,27 +12,31 @@ export default async function IndexPage() {
 	});
 
 	return (
-		<MotionOpacityAnimation duration='0.5' delay='0'>
-			<main className='mt-5 container mx-auto min-h-screen max-w-5xl p-8 sm:px-32 md:px-48 lg:px-20'>
-				<div className=''>
-					<h1 className='text-4xl font-bold mb-8'>Posts</h1> <hr />
-				</div>
-				<ul className='grid lg:grid-cols-2 gap-4'>
-					{posts.map((post, index) => (
-						<li key={post._id} className='lg:first:row-span-2'>
-							<BlogCard
-								title={post.title}
-								categories={post.categories && post.categories}
-								publishedAt={post.publishedAt}
-								imgSrc={post.mainImage && post.mainImage}
-								imgAlt={post.mainImage && post.mainImage}
-								slug={post.slug.current}
-								isFeatured={index === 0}
-							/>
-						</li>
-					))}
-				</ul>
-			</main>
-		</MotionOpacityAnimation>
+		<>
+			{' '}
+			<ReactLenis root />
+			<MotionOpacityAnimation duration='0.5' delay='0'>
+				<main className='mt-5 container mx-auto min-h-screen max-w-5xl p-8 sm:px-32 md:px-48 lg:px-20'>
+					<div className=''>
+						<h1 className='text-4xl font-bold mb-8'>Posts</h1> <hr />
+					</div>
+					<ul className='grid lg:grid-cols-2 gap-4'>
+						{posts.map((post, index) => (
+							<li key={post._id} className='lg:first:row-span-2'>
+								<BlogCard
+									title={post.title}
+									categories={post.categories && post.categories}
+									publishedAt={post.publishedAt}
+									imgSrc={post.mainImage && post.mainImage}
+									imgAlt={post.mainImage && post.mainImage}
+									slug={post.slug.current}
+									isFeatured={index === 0}
+								/>
+							</li>
+						))}
+					</ul>
+				</main>
+			</MotionOpacityAnimation>
+		</>
 	);
 }
