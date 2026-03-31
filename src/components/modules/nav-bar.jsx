ef7a10856caf7f44ea35b559eeea565fad5da5d1
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-03-31 23:54:31 +0800

    styling(nav): Tweak backdrop intensity

--- src/components/modules/nav-bar.jsx
+++ src/components/modules/nav-bar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
 	return (
 		<>
 			<div className='fixed w-full flex flex-col  z-99'>
-				<div className='px-10 h-19 backdrop-blur-md flex justify-center sm:justify-between items-center bg-background/30 border-b'>
+				<div className='px-10 h-19 backdrop-blur-2xl flex justify-center sm:justify-between items-center bg-background/40 border-b'>
 					<Link href='/'>
 						<span className=' font-barcode text-4xl text-gray-700'>TAN QI YANG</span>
 					</Link>
