08a826eb845fd65008cf441070fc0beb8642b72e
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-04-24 11:14:03 +0800

    feat: Add fade in animation to some pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,16 @@
+import MotionOpacityAnimation from '@/components/animations/motion-opacity';
 import AboutSection from '@/components/sections/about';
 import TechstackSection from '@/components/sections/techstack';
 import ReactLenis from 'lenis/react';
 
 export default function AboutPage() {
 	return (
-		<div className='w-full flex flex-col gap-5 justify-center items-center'>
-			<ReactLenis />
-			<TechstackSection />
-			<AboutSection />
-		</div>
+		<MotionOpacityAnimation duration='0.5' delay='0'>
+			<div className='w-full flex flex-col gap-5 justify-center items-center'>
+				<ReactLenis />
+				<TechstackSection />
+				<AboutSection />
+			</div>
+		</MotionOpacityAnimation>
 	);
 }
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,11 +1,14 @@
-import ProjectSection from "@/components/sections/projects";
-import ReactLenis from "lenis/react";
+import MotionOpacityAnimation from '@/components/animations/motion-opacity';
+import ProjectSection from '@/components/sections/projects';
+import ReactLenis from 'lenis/react';
 
 export default function Projects() {
-  return (
+	return (
 		<div className='w-full flex justify-center'>
 			<ReactLenis />
+			<MotionOpacityAnimation duration='0.5' delay='0'>
 				<ProjectSection />
-			</div>
-		);
+			</MotionOpacityAnimation>
+		</div>
+	);
 }
