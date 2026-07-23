4a818d725ec71de459c2a5d5d6c49a7c5fae4074
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-07-23 10:47:16 +0800

    Update client.js

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -3,8 +3,10 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+	projectId,
+	dataset,
+	apiVersion,
+	useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+	browserToken: false,
+	serverToken: false,
+});
