a97159852aab2c410e1858c9e0b4ba776b39dd4e
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-08-09 21:51:34 +0800

    feat: change aspect ratio of blog cards

--- src/components/modules/blog-card.jsx
+++ src/components/modules/blog-card.jsx
@@ -16,13 +16,13 @@ export default function BlogCard({title, categories, publishedAt, imgSrc, imgAlt
 					className={`cursor-pointer dark:bg-foreground/5 border border-background dark:border-foreground/10 w-full items-stretch ${isFeatured === true ? 'h-full' : 'lg:flex-row'} rounded-xl`}
 				>
 					<div
-						className={`relative h-35 w-full shrink-0 overflow-hidden rounded-md ${isFeatured === true ? 'h-45' : 'lg:h-30 lg:w-30'}`}
+						className={`relative h-35 w-full shrink-0 overflow-hidden rounded-md ${isFeatured === true ? 'h-65' : 'lg:h-40 lg:w-50'}`}
 					>
 						{imgSrc ? (
 							<Image
 								loader={imageLoader}
 								loading='eager'
-								className='pointer-events-none absolute inset-0 h-full w-full scale-125 object-cover select-none'
+								className='pointer-events-none absolute inset-0 h-full w-full scale-100 object-cover select-none'
 								src={urlFor(imgSrc.asset._ref).url()}
 								alt={imgAlt.alt}
 								fill={true}
@@ -39,7 +39,7 @@ export default function BlogCard({title, categories, publishedAt, imgSrc, imgAlt
 							<h6 className='ml-1 font-jetbrains text-foreground/45 dark:text-foreground/35'>
 								<DateFromNow type='short' date={publishedAt} />
 							</h6>
-							<Card.Title className='text-xl md:text-2xl font-jetbrains text-foreground/85 text-balance break-all'>
+							<Card.Title className='text-xl md:text-2xl font-jetbrains text-foreground/85 text-balance'>
 								{title}
 							</Card.Title>
 						</Card.Header>
