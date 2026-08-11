11d87edfa5b4bc2404a5733aacc93c4b7ed5bdea
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-08-11 12:22:09 +0800

    feat: update footer and add cc license

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -1,9 +1,34 @@
+import Link from 'next/link';
+import Logo from '../modules/website-logo';
+import { Separator } from '@heroui/react';
 
 export default function Footer() {
 	return (
 		<>
-			<div className='w-full h-50 flex justify-center items-center text-foreground/70'>
-				<h6>Made Without Love 💔</h6>
+			<div className='w-full h-50 px-10 py-50 md:p-50 gap-20 flex flex-col justify-around items-center text-foreground/70'>
+				<div className='flex flex-row gap-3 justify-center items-center'>
+					<Logo />
+					<Separator orientation='vertical' />
+					<Link href='/privacy' className='hover:underline'>
+						Privacy
+					</Link>{' '}
+					<Separator orientation='vertical' />
+					<Link href='/about' className='hover:underline'>
+						About
+					</Link>
+				</div>
+				<div className='flex flex-row gap-3 justify-center text-center'>
+					<p className='text-foreground/50 text-sm md:text-base text-balance'>
+						© 2026 Tan Qi Yang. Content licensed under{' '}
+						<a
+							className='hover:underline'
+							href='https://creativecommons.org/licenses/by-nc-nd/4.0/'
+						>
+							CC BY-NC-ND 4.0
+						</a>
+						.
+					</p>
+				</div>
 			</div>
 		</>
 	);
