bfa79eb1e6bb0fa971b14816c37567caa23c5299
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-09-01 17:21:30 +0800

    sec(config): add security headers and strict CSP

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,51 @@
 /** @type {import('next').NextConfig} */
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const ContentSecurityPolicy = `
+    default-src 'none';
+    script-src 'self' 'unsafe-inline' ${isDev ? "'unsafe-eval'" : ''} https://*.umami.is https://web3forms.com https://hcaptcha.com https://*.hcaptcha.com;
+    frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com;
+		style-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com;
+    img-src 'self' blob: data: https://cdn.sanity.io https://*.hcaptcha.com;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    connect-src 'self' https://gateway.umami.is https://hcaptcha.com https://*.hcaptcha.com https://api.web3forms.com;
+    form-action 'self' https://api.web3forms.com;
+    frame-ancestors 'none'; 
+    upgrade-insecure-requests;
+`;
+
+const securityHeaders = [
+	{
+		key: 'Content-Security-Policy',
+		value: ContentSecurityPolicy.replace(/\s+/g, ' ').trim(),
+	},
+	{
+		key: 'X-Frame-Options',
+		value: 'DENY',
+	},
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	},
+	{
+		key: 'Referrer-Policy',
+		value: 'strict-origin-when-cross-origin',
+	},
+	{
+		key: 'Permissions-Policy',
+		value: 'camera=(), microphone=(), geolocation=()',
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=63072000; includeSubDomains; preload',
+	},
+];
+
 const nextConfig = {
 	reactStrictMode: true,
-	allowedDevOrigins: ['192.168.0.188'],
 	env: {
 		BASE_URL: process.env.BASE_URL,
 	},
@@ -14,7 +58,19 @@ const nextConfig = {
 				pathname: '/**',
 			},
 		],
-		qualities: [25, 50, 75],
+	},
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: securityHeaders,
+			},
+		];
+	},
+	experimental: {
+		sri: {
+			algorithm: 'sha256',
+		},
 	},
 };
 
